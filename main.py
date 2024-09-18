6cbf310d79e1f9fa293189769800fa8cc29e955b
Maciej Bieliński <121232774+MaciejBielinski-oc@users.noreply.github.com>
2024-09-18 22:22:20 +0200

    First commit with basic version of calculator. User can add, sumstract, multiply and divide as many numbers as want, but only one equation (one symbol) for one request. All is happening in console

---
+++ main.py
@@ -0,0 +1,53 @@
+
+print ("Witamy w kalkulatorze. Wpisz działanie dodawania (+), odejmowania (-), mnożenia (*) lub dzielenia (/) wybranych liczb. Można wybrać tylko jedno działanie naraz")
+
+user_eq=input("Podaj swoje działanie: ")
+
+number_plus=user_eq.count("+")
+number_minus=user_eq.count("-")
+number_div=user_eq.count("/")
+number_mul=user_eq.count("*")
+
+def add(numbers):
+    result = 0
+    for i in numbers:
+        result = result+int(i)
+    return result
+
+def sub(numbers):
+    result = int(numbers[0])
+    for i in numbers:
+        result = result-int(i)
+    return result
+
+def mul(numbers):
+    result = 1
+    for i in numbers:
+        result = result*int(i)
+    return result
+
+def div(numbers):
+    result = 0
+    count=0
+    for i in numbers:
+        count+=1
+        if count==1:
+            result=int(i)
+        else:
+            result = result/int(i)
+    return result
+
+if "+" in user_eq:
+    user_var = user_eq.split("+")
+    print (add(user_var))
+elif "-" in user_eq:
+    user_var = user_eq.split("-")
+    print(sub(user_var))
+elif "*" in user_eq:
+    user_var = user_eq.split("*")
+    print(mul(user_var))
+elif "/" in user_eq:
+    user_var = user_eq.split("/")
+    print(div(user_var))
+else:
+    print ("Nieznany symbol działania")
